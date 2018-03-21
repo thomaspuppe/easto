5ece999467f59104abed806f35888465958daddd
Thomas Puppe <thomas.puppe@zeit.de>
2018-03-21 08:52:46 +0100

    iterate over content files

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const fs = require('fs')
+
 console.log('Hello World')
+console.log('Reading content directory')
+
+fs.readdirSync('./content').forEach(function(filename) {
+  const filePath = './content/' + filename
+  console.log('- ' + filePath)
+})
