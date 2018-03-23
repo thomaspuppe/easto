169aa8190461c9571e4748ced8e7baa3e7629f7b
Thomas Puppe <thomas.puppe@zeit.de>
2018-03-23 21:41:50 +0100

    Take cli arguments

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const fs = require('fs')
 const marked = require('marked')
 const yaml = require('yaml-front-matter')
 
+// TODO: Optimize this! Wozu hamma denn deconstruction, map, usw?
+let args = new Map()
+process.argv.forEach(function(val) {
+  if (val.startsWith('--')) {
+    val = val.slice(2)
+    if (val.indexOf('=') > -1) {
+      const valArray = val.split('=')
+      args.set(valArray[0], valArray[1])
+    }
+  }
+})
+
+const CONTENT_DIR = args.get('content') || 'content'
+const OUTPUT_DIR = args.get('output') || 'output'
+
 console.log('Reading content directory')
 
-fs.readdirSync('./content').forEach(filename => {
-  const filePath = './content/' + filename
+fs.readdirSync(`./${CONTENT_DIR}`).forEach(filename => {
+  const filePath = `./${CONTENT_DIR}/${filename}`
   console.log('- ' + filePath)
 
   const fileContent = fs.readFileSync(filePath, {
@@ -30,7 +45,7 @@ fs.readdirSync('./content').forEach(filename => {
     )
   }
 
-  const targetPath = './output/' + filename.replace('.md', '.html')
+  const targetPath = `./${OUTPUT_DIR}/` + filename.replace('.md', '.html')
   fs.writeFileSync(targetPath, targetContent)
   console.log('  - wrote file: ' + targetPath)
 })
